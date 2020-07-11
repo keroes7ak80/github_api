810b4ee76d60f28b011b7477ac4836efbd648dc2
keroes7ak <kero_es7ak@hotmail.com>
2020-07-11 08:11:43 +0200

    first commit

---
+++ app/Http/Controllers/LanguagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class LanguagesController extends Controller
+{
+    public function showLanguages()
+    {
+    	$response = Http::get('https://api.github.com/search/repositories?q=created:>2020-07-10&sort=stars&order=desc&per_page=100');
+    	$repos=json_decode($response->body());
+ 
+    	return  response()->json($repos);
+	}
+}
