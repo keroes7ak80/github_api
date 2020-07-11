1a626fff9721757dee2e452f199508b6f70c545b
keroes7ak <kero_es7ak@hotmail.com>
2020-07-11 09:27:48 +0200

    Adding getRepos and getLanguages functions

--- app/Http/Controllers/LanguagesController.php
+++ app/Http/Controllers/LanguagesController.php
@@ -7,10 +7,28 @@ use Illuminate\Support\Facades\Http;
 
 class LanguagesController extends Controller
 {
+	private function getRepos()
+	{
+		$response = Http::get('https://api.github.com/search/repositories?q=created:>2020-07-10&sort=stars&order=desc&per_page=100');
+    	$repos=json_decode($response->body());
+    	$repos=$repos->items;
+
+    	return $repos;
+	}
+
+	private function getLanguages()
+	{
+		$repos=$this->getRepos();
+    	$languages=array_column($repos, 'language'); // Get languages of all repos
+    	$languages=array_filter($languages); // Remove nulls
+    	$languages = array_values($languages); 
+    	return $languages;
+	}
+
+
     public function showLanguages()
     {
-    	$response = Http::get('https://api.github.com/search/repositories?q=created:>2020-07-10&sort=stars&order=desc&per_page=100');
-    	$repos=json_decode($response->body());
-    	return  response()->json($repos);
+    	return  response()->json($this->getLanguages());
 	}
+
 }
